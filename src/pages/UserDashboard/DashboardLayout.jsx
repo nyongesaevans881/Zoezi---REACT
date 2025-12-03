0b3c78f2bb8d1f52b0a002a32999db3a60522e65
Nyongesa Evans <nyongesaevans881@gmai.com>
2025-12-03 23:59:03 +0300

    Login Update more

--- src/pages/UserDashboard/DashboardLayout.jsx
+++ src/pages/UserDashboard/DashboardLayout.jsx
@@ -133,7 +133,7 @@ export default function DashboardLayout({ userType, children }) {
         <TopNav />
 
         {/* Main Content */}
-        <main className="flex-1 p-4 sm:p-6 md:p-8 overflow-y-auto">
+        <main className="flex-1 p-4 sm:p-1 md:p-8 overflow-y-auto max-md:p-1">
           <div className="max-w-7xl mx-auto">
             {children({
               userData,
